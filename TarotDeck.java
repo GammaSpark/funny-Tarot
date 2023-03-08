d86d8ac76a0f9b2c71032842a88b6cba51d46e88
GammaSpark <jtguerrero25@gmail.com>
2023-03-08 09:32:49 -0600

    made for fun

---
+++ TarotDeck.java
@@ -0,0 +1,181 @@
+import java.util.*;
+
+public class TarotDeck {
+    private String card;
+    private boolean inverted;
+
+    public TarotDeck() {
+    }
+
+    public String pull() {
+        Random random = new Random(2);
+        if(random.nextInt() == 0){
+            card = majorArcana();
+        }
+        else card = minorArcana();
+        return card;
+
+    }
+    private boolean isInverted(){
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    public String pullMajor(){
+        return majorArcana();
+    }
+
+    private String majorArcana() {
+        Random random = new Random();
+        boolean getInverted = random.nextBoolean();
+        String inverted = "";
+        int arcana = random.nextInt(22);
+        String arcanaString = "The Fool";
+        
+        if (isInverted()){
+            inverted = "Inverted ";
+        }
+        switch(arcana){
+            case 0:{
+                arcanaString = "Fool";
+                break;
+            }
+            case 1:{
+                arcanaString = "Magician";
+                break;
+            }
+            case 2:{
+                arcanaString = "High Priestess";
+                break;
+            }
+            case 3:{
+                arcanaString = "Empress";
+                break;
+            }
+            case 4:{
+                arcanaString = "Emperor";
+                break;
+            }
+            case 5:{
+                arcanaString = "Hierophant";
+                break;
+            }
+            case 6:{
+                arcanaString = "Lovers";
+                break;
+            }
+            case 7:{
+                arcanaString = "Chariot";
+                break;
+            }
+            case 8:{
+                arcanaString = "Strength";
+                break;
+            }
+            case 9:{
+                arcanaString = "Hermit";
+                break;
+            }
+            case 10:{
+                arcanaString = "Wheel of Fortune";
+                break;
+            }
+            case 11:{
+                arcanaString = "Justice";
+                break;
+            }
+            case 12:{
+                arcanaString = "Hanged Man";
+                break;
+            }
+            case 13:{
+                arcanaString = "Death";
+                break;
+            }
+            case 14:{
+                arcanaString = "Temperance";
+                break;
+            }
+            case 15:{
+                arcanaString = "Devil";
+                break;
+            }
+            case 16:{
+                arcanaString = "Tower";
+                break;
+            }
+            case 17:{
+                arcanaString = "Star";
+                break;
+            }
+            case 18:{
+                arcanaString = "Moon";
+                break;
+            }
+            case 19:{
+                arcanaString = "Sun";
+                break;
+            }
+            case 20:{
+                arcanaString = "Judgement";
+                break;
+            }
+            case 21:{
+                arcanaString = "World";
+                break;
+            }
+        }
+
+        return "The " + inverted + arcanaString;
+    }
+
+    private String minorArcana() {
+        Random random = new Random();
+        int suite = random.nextInt(4);
+        int number = random.nextInt(14);
+        String suiteString = "";
+        String numberString = "";
+        switch (suite) {
+            case 0: {
+                suiteString = "Wands";
+                break;
+            }
+            case 1: {
+                suiteString = "Cups";
+                break;
+            }
+            case 2: {
+                suiteString = "Swords";
+                break;
+            }
+            case 3: {
+                suiteString = "Pentacles";
+                break;
+            }
+        }
+        switch (number) {
+            case 0: {
+                numberString = "Queen";
+                break;
+            }
+            case 1: {
+                numberString = "King";
+                break;
+            }
+            case 2: {
+                numberString = "Knight";
+                break;
+            }
+            case 3: {
+                numberString = "Page";
+            }
+            case default: {
+                number = number - 3;
+                numberString = "" + number;
+                break;
+            }
+        }
+        return numberString + " of " + suiteString;
+    }
+
+}
---
+++ TarotReader.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class TarotReader {
+    public static void main(String[] args) {
+        TarotDeck deck = new TarotDeck();
+        System.out.println(deck.pullMajor());
+    }
+
+
+}
