78cd7a07e757da32fffdde8045f5589fd6d0f370
GammaSpark <jtguerrero25@gmail.com>
2023-03-08 09:35:43 -0600

    Reworking card inverstion

--- TarotReader.java
+++ TarotReader.java
@@ -1,9 +1,12 @@
 import java.util.Random;
 
+import javax.swing.JFrame;
+
 public class TarotReader {
     public static void main(String[] args) {
         TarotDeck deck = new TarotDeck();
         System.out.println(deck.pullMajor());
+        
     }
 
 
